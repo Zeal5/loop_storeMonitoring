0cca1fb3967dbfd9bd4dd228b5166636c6982f21
Zeal5 <tassadiqshakeel5@gmail.com>
2023-08-09 14:25:23 +0500

    on start_up completed

--- on_server_startup.py
+++ on_server_startup.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 from database.connections import get_connection
-from asyncpg.exceptions import UndefinedTableError, PostgresError
 
 
 async def add_days_of_week() -> None:
@@ -24,7 +23,7 @@ async def add_days_of_week() -> None:
         async with get_connection() as conn:
             async with conn.transaction():
                 for i in zip(days_names_columns, days_number_columns):
-                    await conn.execute(query,i[0],i[1])
+                    await conn.execute(query, i[0], i[1])
         print("Days of the week added to database")
 
 
@@ -39,43 +38,82 @@ async def add_stores_data(root_dir: os.PathLike) -> None:
     bq_file_df = pd.read_csv(bq_file_path)
 
     # add stores from store_time_zone.csv
-    columns = ['store_id', 'local_time_zone']
+    columns = ["store_id", "local_time_zone"]
     if await check_if_table_empty("stores"):
         records = [tuple(x) for x in bq_file_df.values]
         async with get_connection() as conn:
             async with conn.transaction():
-                await conn.copy_records_to_table("stores", records = records,columns = columns)
+                await conn.copy_records_to_table(
+                    "stores", records=records, columns=columns
+                )
                 print("stores added from bq_csv successfully")
-
 
     # add stores from operations time csv ( to add missing stores)
     stores_in_menu_csv = set(int(x[0]) for x in menu_file_df.values)
     stores_in_bq_csv = set(int(x[0]) for x in bq_file_df.values)
 
     stores_not_in_db = stores_in_menu_csv - stores_in_bq_csv
-    print(stores_not_in_db)
-    stores_in_bq_but_not_in_db = [(x,'America/Chicago') for x in stores_not_in_db] 
+    stores_in_bq_but_not_in_db = [(x, "America/Chicago") for x in stores_not_in_db]
     async with get_connection() as conn:
         async with conn.transaction():
             for i in stores_in_bq_but_not_in_db:
-                await conn.execute("""
+                await conn.execute(
+                    """
                              INSERT INTO stores (store_id,local_time_zone) values($1,$2)
                              ON CONFLICT (store_id) DO NOTHING 
-                             """,i[0],i[1])
-            
+                             """,
+                    i[0],
+                    i[1],
+                )
+
             print("remaining stores added from menu_csv successfully")
-    print(f"len of stores in menue_csv = {len(stores_in_menu_csv)}")
-    print(f"len of stores in db = {len(stores_in_bq_csv)}")
-    print(f"len of stores not in db = {len(stores_not_in_db)}")
+    # print(f"len of stores in menue_csv = {len(stores_in_menu_csv)}")
+    # print(f"len of stores in db = {len(stores_in_bq_csv)}")
+    # print(f"len of stores not in db = {len(stores_not_in_db)}")
+
+
+async def add_business_hours(root_dir: os.PathLike):
+    menu_file_path = os.path.join(root_dir, "Menu hours.csv")
+    menu_file_df = pd.read_csv(menu_file_path)
+    id_in_menu_file = list(map(str, menu_file_df['store_id'].unique()))
+
+    async with get_connection() as conn:
+        async with conn.transaction():
+            query_for_store_id = f"SELECT id,store_id FROM stores WHERE store_id IN ({','.join(id_in_menu_file)});"
+            result = await conn.fetch(query_for_store_id)
+            map_id = {int(record['store_id']):record['id'] for record in result}
+
+            menu_file_df['store_id'] = menu_file_df['store_id'].map(map_id)
+
+            query_for_days_id = f"SELECT id, day_number FROM days_of_week"
+            map_days_to_Number = {record['day_number']: record['id'] for record in await conn.fetch(query_for_days_id)}
+
+            menu_file_df['day'] = menu_file_df['day'].map(map_days_to_Number)
 
 
 
 
-        
+            menu_file_df['start_time_local'] = pd.to_datetime(menu_file_df['start_time_local'], format='%H:%M:%S')
+            menu_file_df['end_time_local'] = pd.to_datetime(menu_file_df['end_time_local'], format='%H:%M:%S')
+
+            data = menu_file_df.values.tolist()
+            insert_query =f"""INSERT INTO business_hours (  store_id, 
+                                                            day_of_week_id, 
+                                                            bussiness_start_time,   
+                                                            bussiness_end_time) 
+                                                            SELECT $1, $2, $3::time, $4::time
+                                                            WHERE NOT EXISTS (
+                                                            SELECT 1 FROM business_hours
+                                                            WHERE store_id = $1 AND day_of_week_id = $2) """
+
+
+            await conn.executemany(insert_query, data)
+              
+
+
 
 
 async def check_if_table_empty(table_name: str) -> bool:
-
     """Takes in a table name\n
     returns True if table is empty else False"""
 
@@ -86,10 +124,9 @@ async def check_if_table_empty(table_name: str) -> bool:
             return False if await conn.fetchrow(query) else True
 
 
-
 async def create_tables(root_dir: os.PathLike) -> None:
-    """ Create tables from Schema.sql \n
-    Returns None """
+    """Create tables from Schema.sql \n
+    Returns None"""
     schema_file = os.path.join(root_dir, "database", "schema.sql")
 
     with open(schema_file, "r") as schemas:
@@ -115,18 +152,11 @@ async def on_startup() -> None:
     # Fill out days_of_week table
     await add_days_of_week()
 
-    # Fill out stores table 
-
+    # Fill out stores table
     await add_stores_data(root_dir)
 
-
     # Fill business_hours table
-
-
-    
-    
-
-    
+    # await add_business_hours(root_dir) @TODO uncomment (takes too long)
 
     stores_is_empty = await check_if_table_empty("stores")
 
